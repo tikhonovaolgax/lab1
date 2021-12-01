476facc74d99719b26b7f3a5b25e9da3b41f37ea
Olga Tikhonova <tikhonovaolgax@gmail.com>
2021-12-01 17:01:08 +0300

    Начальная фиксация

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>lab1</title>
+		<link rel="stylesheet" href="mycas.css">
+		<link rel="shortcut icon" href="favicon.ico">
+	</head>
+	<body>
+		<div class="mydivl">
+			<?php
+				echo "Вариант 26 (6) Вычислить среднее арифметическое трех чисел<br/>";
+				echo "Результат среднего арифметического 342645724, 213223462 и 24574257457 равен: ";
+				echo 342645724+24574257452;
+			?>
+		</div>
+	</body>
+</html>
